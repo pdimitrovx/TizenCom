54776a6c86c5049da80394f6d802a26a5a4c513e
Petar Dimitrov <pid4@aber.ac.uk>
2019-04-20 13:56:01 +0100

    On going work on main, still not fixed, On create is now tidy

--- app/src/main/java/uk/ac/aber/dcs/pid4/tizencom/MainActivity.java
+++ app/src/main/java/uk/ac/aber/dcs/pid4/tizencom/MainActivity.java
@@ -24,193 +24,199 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
-        private TextView mTextMessage;
-        private Button start_Btn, connect_test_Btn;
-        private boolean isServiceBound = false;
-        private Service_SAP SAPservice = null;
-        private boolean start_Btn_clicked = false;
-        Service_SAP mConsumerService = null;
-        private static MessageAdapter mMessageAdapter; //todo mesasge stuff, describe ite etc
-        private ListView mMessageListView; //todo message stuff
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+    private Button start_Btn, connect_test_Btn;
+    private boolean isServiceBound = false;
+    private boolean start_Btn_clicked = false;
+    private Service_SAP mConsumerService = null;
 
+    private static MessageAdapter mMessageAdapter; //todo mesasge stuff, describe ite etc
+    private ListView mMessageListView; //todo message stuff
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Bind service
+        isServiceBound = bindService(new Intent(MainActivity.this,
+                Service_SAP.class), mConnection, Context.BIND_AUTO_CREATE);
+
+
+        //todo rename and tidy list view
+        mMessageListView = findViewById(R.id.lvMessage);
+        mMessageAdapter = new MessageAdapter();
+        mMessageListView.setAdapter(mMessageAdapter);
+
+        //todo navigatio drawer stuff, mayeb replace with fragment
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Initialize spinners
+        Spinner sensor_selector = findViewById(R.id.sensor_spinner);
+        Spinner time_selector = findViewById(R.id.time_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_sensor = ArrayAdapter.createFromResource(this,
+                R.array.sensors_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter_time = ArrayAdapter.createFromResource(this,
+                R.array.time_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter_sensor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_time.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the sensor_selector
+        sensor_selector.setAdapter(adapter_sensor);
+        time_selector.setAdapter(adapter_time);
+
+        //set up the start button
+        start_Btn = findViewById(R.id.start_btn);
+
+        //setup the connection test button
+        connect_test_Btn = findViewById(R.id.connectTest);
+
+    }
+
+
+    public void mOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.connectTest: {
+                if (isServiceBound == true && mConsumerService != null) {
+                    mConsumerService.findPeers();
+                    start_Btn_clicked = false;
+                    // mConsumerService.sendData("Hello Message!");
+                }
+                break;
+            }
+            case R.id.start_btn: {
+                if (isServiceBound == true && start_Btn_clicked == false && mConsumerService != null) {
+                    if (mConsumerService.sendData("Hello Message!") != -1) {
+                        //todo wtf is it sendButtonClicked = true;
+                    } else {
+                        //todo wtfff sendButtonClicked = false;
+                    }
+                }
+                break;
+            }
+            default:
+        }
+    }
+
+    private final ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mConsumerService = ((Service_SAP.LocalBinder) service).getService();
+            //updateTextView("onServiceConnected");
+        }
 
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        public void onServiceDisconnected(ComponentName className) {
+            mConsumerService = null;
+            isServiceBound = false;
+            //updateTextView("onServiceDisconnected");
+        }
+    };
 
-            mMessageListView = (ListView) findViewById(R.id.lvMessage);
-            mMessageAdapter = new MessageAdapter();
-            mMessageListView.setAdapter(mMessageAdapter);
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-            mTextMessage = (TextView) findViewById(R.id.message);
-            BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //todo, tidy this code up and deploy fragments / tabs or remove alltogether
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
 
-            Spinner sensor_selector = findViewById(R.id.sensor_spinner);
-            Spinner time_selector = findViewById(R.id.time_spinner);
+    //todo message nested class have a look
+    static final class Message {
+        String data;
 
-            // Create an ArrayAdapter using the string array and a default spinner layout
-            ArrayAdapter<CharSequence> adapter_sensor = ArrayAdapter.createFromResource(this,
-                    R.array.sensors_array, android.R.layout.simple_spinner_item);
-            ArrayAdapter<CharSequence> adapter_time = ArrayAdapter.createFromResource(this,
-                    R.array.time_array, android.R.layout.simple_spinner_item);
+        public Message(String data) {
+            super();
+            this.data = data;
+        }
+    }
 
-            // Specify the layout to use when the list of choices appears
-            adapter_sensor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            adapter_time.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    //todo MESSAGE STUFF URGENT
+    class MessageAdapter extends BaseAdapter {
+        private static final int MAX_MESSAGES_TO_DISPLAY = 20;
+        private List<Message> mMessages;
 
-            // Apply the adapter to the sensor_selector
-            sensor_selector.setAdapter(adapter_sensor);
-            time_selector.setAdapter(adapter_time);
-
-            //set up the start button
-            start_Btn = findViewById(R.id.start_btn);
-
-            //setup the connection test button
-            connect_test_Btn = findViewById(R.id.connectTest);
-
-            // Bind service
-            isServiceBound = bindService(new Intent(MainActivity.this, Service_SAP.class), mConnection, Context.BIND_AUTO_CREATE);
+        public MessageAdapter() {
+            mMessages = Collections.synchronizedList(new ArrayList<Message>());
         }
 
-
-        public void mOnClick(View v) {
-            switch (v.getId()) {
-                case R.id.connectTest: {
-                    if (isServiceBound == true && mConsumerService != null) {
-                        mConsumerService.findPeers();
-                        start_Btn_clicked = false;
-                        // mConsumerService.sendData("Hello Message!");
-                    }
-                    break;
-                }
-                case R.id.start_btn: {
-                    if (isServiceBound == true && start_Btn_clicked == false && mConsumerService != null) {
-                        if (mConsumerService.sendData("Hello Message!") != -1) {
-                            //todo wtf is it sendButtonClicked = true;
-                        } else {
-                            //todo wtfff sendButtonClicked = false;
-                        }
-                    }
-                    break;
-                }
-                default:
+        void addMessage(final Message msg) {
+            // Runnable newTask = (new Runnable() {
+            //  @Override
+            //  public void run() {
+            if (mMessages.size() == MAX_MESSAGES_TO_DISPLAY) {
+                mMessages.remove(0);
+                mMessages.add(msg);
+            } else {
+                mMessages.add(msg);
             }
+            notifyDataSetChanged();
+            mMessageListView.setSelection(getCount() - 1);
+            //   }
+            //});
         }
 
-        private final ServiceConnection mConnection = new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName className, IBinder service) {
-                mConsumerService = ((Service_SAP.LocalBinder) service).getService();
-                //updateTextView("onServiceConnected");
+        void clear() {
+            // runOnUiThread(new Runnable() {
+            ///  @Override
+            //public void run() {
+            mMessages.clear();
+            notifyDataSetChanged();
+            // }
+            // });
+        }
+
+        @Override
+        public int getCount() {
+            return mMessages.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mMessages.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflator = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View messageRecordView = null;
+            if (inflator != null) {
+                messageRecordView = inflator.inflate(R.layout.message, null); //todo sort out what message does, item in list?
+                TextView tvData = (TextView) messageRecordView.findViewById(R.id.tvData);
+                Message message = (Message) getItem(position);
+                tvData.setText(message.data);
             }
-
-            @Override
-            public void onServiceDisconnected(ComponentName className) {
-                mConsumerService = null;
-                isServiceBound = false;
-                //updateTextView("onServiceDisconnected");
-            }
-        };
-
-        private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-                = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-            //todo, tidy this code up and deploy fragments / tabs or remove alltogether
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_home:
-                        mTextMessage.setText(R.string.title_home);
-                        return true;
-                    case R.id.navigation_dashboard:
-                        mTextMessage.setText(R.string.title_dashboard);
-                        return true;
-                    case R.id.navigation_notifications:
-                        mTextMessage.setText(R.string.title_notifications);
-                        return true;
-                }
-                return false;
-            }
-        };}
-
-//todo message nested class have a look
-static final class Message {
-    String data;
-
-    public Message(String data) {
-        super();
-        this.data = data;
+            return messageRecordView;
+        }
     }
 }
-
-//todo MESSAGE STUFF URGENT
-class MessageAdapter extends BaseAdapter {
-    private static final int MAX_MESSAGES_TO_DISPLAY = 20;
-    private List<Message> mMessages;
-
-    public MessageAdapter() {
-        mMessages = Collections.synchronizedList(new ArrayList<Message>());
-    }
-
-    void addMessage(final Message msg) {
-        // Runnable newTask = (new Runnable() {
-        //  @Override
-        //  public void run() {
-        if (mMessages.size() == MAX_MESSAGES_TO_DISPLAY) {
-            mMessages.remove(0);
-            mMessages.add(msg);
-        } else {
-            mMessages.add(msg);
-        }
-        notifyDataSetChanged();
-        mMessageListView.setSelection(getCount() - 1);
-        //   }
-        //});
-    }
-
-    void clear() {
-        // runOnUiThread(new Runnable() {
-        ///  @Override
-        //public void run() {
-        mMessages.clear();
-        notifyDataSetChanged();
-        // }
-        // });
-    }
-
-    @Override
-    public int getCount() {
-        return mMessages.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return mMessages.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflator = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View messageRecordView = null;
-        if (inflator != null) {
-            messageRecordView = inflator.inflate(R.layout.message, null); //todo sort out what message does, item in list?
-            TextView tvData = (TextView) messageRecordView.findViewById(R.id.tvData);
-            Message message = (Message) getItem(position);
-            tvData.setText(message.data);
-        }
-        return messageRecordView;
-    }
-}
-
 //added the service connection thing that is needed in order to set up the sercive bound thing. Explain taht
 //added         android:onClick="mOnClick" to each button so the trigger that method that has a switch case statement in it!
